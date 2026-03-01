80b8ad42380d5e73e52039474d56d0fbc086cbb3
Ruhi K <ruhik8789@gmail.com>
2026-03-01 23:54:35 +0530

    pushed the changes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,53 @@
-import React, { useState }from 'react'
+import React, { useState } from "react";
 
 const App = () => {
   const [input, setInput] = useState("");
   const [response, setResponse] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const askAI = async () => {
-    const res = await fetch("https://openrouter.ai/api/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        Authorization:
-          "Bearer Your_API_Key",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        model: "openai/gpt-4o-mini",
-        messages: [{ role: "user", content: input }],
-      }),
-    });
+    if (!input.trim()) return;
+    setLoading(true);
 
-    const data = await res.json();
-    setResponse(data.choices[0].message.content);
+    try {
+      const res = await fetch("https://openrouter.ai/api/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer Your_API_Key",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          model: "openai/gpt-4o-mini",
+          messages: [{ role: "user", content: input }],
+        }),
+      });
+
+      if (!res.ok) throw new Error("API Failed...");
+
+      const data = await res.json();
+      setLoading(false);
+      setResponse(data.choices[0].message.content);
+    } catch (error) {
+      setLoading(false);
+      setResponse("Something went wrong");
+    } finally {
+      setInput("");
+    }
   };
   return (
     <div>
       <h2>AI Interview App</h2>
-      <input value={input} onChange={(e) => setInput(e.target.value)} />
-      <button onClick={askAI}>Ask</button>
+      <input
+        className="padding"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button className="padding marginLeft" disbaled={loading} onClick={askAI}>
+        {loading ? "Loading..." : "Ask AI"}
+      </button>
       <p>{response}</p>
     </div>
   );
-}
+};
 
-export default App
+export default App;
